bc633bc5ab089de27c59ffba93c2a500062b4c19
Aya Naji <naya34045@gmail.com>
2024-12-31 13:53:29 +0100

    crud folder

--- Back-end/FolderService/src/main/java/com/example/FolderService/Services/FolderService.java
+++ Back-end/FolderService/src/main/java/com/example/FolderService/Services/FolderService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FolderService {
@@ -15,4 +16,49 @@ public class FolderService {
     public List<Folder> getAllDossiers() {
         return folderRepository.findAll();
     }
+    public Folder createFolder(Folder folder) {
+        folder.setActive(true);
+        return folderRepository.save(folder);
+    }
+    public Folder updateFolder(Integer numDossier, Folder updatedFolder) {
+        // Rechercher le folder existant
+        Optional<Folder> existingFolderOptional = folderRepository.findById(numDossier);
+        if (existingFolderOptional.isEmpty()) {
+            throw new IllegalArgumentException("Folder avec le numéro " + numDossier + " introuvable.");
+        }
+
+        Folder existingFolder = existingFolderOptional.get();
+
+        // Mettre à jour les champs nécessaires
+        existingFolder.setNomComplet(updatedFolder.getNomComplet());
+        existingFolder.setDateNaissance(updatedFolder.getDateNaissance());
+        existingFolder.setLieuDeNaissance(updatedFolder.getLieuDeNaissance());
+        existingFolder.setSexe(updatedFolder.getSexe());
+        existingFolder.setTypePieceIdentite(updatedFolder.getTypePieceIdentite());
+        existingFolder.setNumPieceIdentite(updatedFolder.getNumPieceIdentite());
+        existingFolder.setAdresse(updatedFolder.getAdresse());
+        existingFolder.setNumTel(updatedFolder.getNumTel());
+        existingFolder.setEmail(updatedFolder.getEmail());
+        existingFolder.setActive(updatedFolder.getActive());
+        existingFolder.setFkIdLaboratoire(updatedFolder.getFkIdLaboratoire());
+
+        // Sauvegarder les modifications
+        return folderRepository.save(existingFolder);
+    }
+
+    public void archiveFolder(Integer numDossier) {
+        // Rechercher le folder par son numéro
+        Optional<Folder> folderOptional = folderRepository.findById(numDossier);
+        if (folderOptional.isEmpty()) {
+            throw new IllegalArgumentException("Folder avec le numéro " + numDossier + " introuvable.");
+        }
+
+        // Mettre à jour le champ "active"
+        Folder folder = folderOptional.get();
+        folder.setActive(false);
+
+        // Sauvegarder les modifications
+        folderRepository.save(folder);
+    }
+
 }
